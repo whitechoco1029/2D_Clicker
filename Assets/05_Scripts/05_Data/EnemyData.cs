4644a4a88fbac8d158f0b503e9fcc6b7deb222d8
leeejinho <james8488@naver.com>
2025-03-27 19:13:14 +0900

    Design: 스테이지 UI 추가

--- Assets/05_Scripts/05_Data/EnemyData.cs
+++ Assets/05_Scripts/05_Data/EnemyData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "new EnemyData", menuName = "Data/EnemyData")]
 public class EnemyData : ScriptableObject
 {
     public float health;
